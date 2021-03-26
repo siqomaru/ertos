bdd9fd39a1f814c78bc0bbb77b8117f8b25bdae2
itdao5 <p130420022@bussan.co.id>
2021-03-26 23:06:53 +0700

    ini gua rubah yaa

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1" name="viewport">
 
-  <title>ERTOS KONSULTASI DOKTER</title>
+  <title>IPUL</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
   
